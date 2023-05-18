7186c7d1949d68c9ba3cb07af5451e6d53f433e2
biswajitbehura12 <biswajitbehura12@gmail.com>
2023-05-18 15:51:18 +0530

    first commit

---
+++ client/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+const Footer = () => {
+    return (
+        <p>Made By Biswajit Behura</p>
+    )
+}
+
+
+export default Footer;
---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: `mongodb+srv://dpuser:dpUser@blog-app2.hfxckhs.mongodb.net/?retryWrites=true&w=majority`
+}
